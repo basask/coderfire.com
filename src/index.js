b15506ae415170a065f9896162c79fbcd272bcac
basask <basask@gmail.com>
2021-11-19 10:11:03 -0300

    feat - First coderfire website version

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/styles/tailwind.css";
+
+// layouts
+
+import Admin from "layouts/Admin";
+import Auth from "layouts/Auth";
+
+// Components
+
+import IndexNavbar from "components/Navbars/IndexNavbar";
+import Footer from "components/Footers/Footer"
+
+// views without layouts
+
+import Landing from "views/Landing.js";
+import Profile from "views/Profile.js";
+import Index from "views/Index.js";
+import SudokuGame from "views/sudoku/Game";
+
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <IndexNavbar fixed />
+    <Switch>
+      {/* add routes with layouts */}
+      <Route path="/admin" component={Admin} />
+      <Route path="/auth" component={Auth} />
+      {/* add routes without layouts */}
+      <Route path="/landing" exact component={Landing} />
+      <Route path="/profile" exact component={Profile} />
+      <Route path="/project/sudoku" exact component={SudokuGame} />
+      <Route path="/" exact component={Index} />
+      {/* add redirect for first page */}
+      <Redirect from="*" to="/" />
+    </Switch>
+    <Footer />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
